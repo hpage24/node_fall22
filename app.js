2e298eeca6a773c6e1e41c80c913fba2f2c876c9
hpage24 <hspage11@gmail.com>
2022-10-17 00:46:25 +0000

    Added MongoDB connection

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
+
 app.use('/static', express.static("public"));
+app.use(express.urlencoded({extended: true}))
 app.set("view engine", "ejs");
 
-app.get('/hello', function(req, res) {
-    res.send('Hello World');
-})
+const mongoDB = 'mongodb+srv://hpage11:Jack$on2411@cluster0.u05i2ze.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error: "))
 
 app.get('/', function(req, res) {
     res.render('todo.ejs');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.content)
 })
 
 app.listen(3000, function(){
