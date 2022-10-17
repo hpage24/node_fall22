95678cea0d9644fca6e2f1db4e41877050ce08cf
hpage24 <hspage11@gmail.com>
2022-10-17 00:12:51 +0000

    Added styles

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+app.use('/static', express.static("public"));
+app.set("view engine", "ejs");
+
+app.get('/hello', function(req, res) {
     res.send('Hello World');
+})
+
+app.get('/', function(req, res) {
+    res.render('todo.ejs');
 })
 
 app.listen(3000, function(){
     console.log('App listening on port 3000')
 })
-
